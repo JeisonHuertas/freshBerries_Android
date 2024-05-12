774399ecbce4993cc0924b8e5765d60b105a1b16
Jeison <jeisonhuertas@gmail.com>
2024-05-11 20:36:21 -0500

    Registro de Proveedor completo

--- app/src/main/java/com/example/freshberries/Activities/ui/proveedores/ProveedoresFragment.kt
+++ app/src/main/java/com/example/freshberries/Activities/ui/proveedores/ProveedoresFragment.kt
@@ -1,15 +1,21 @@
 package com.example.freshberries.Activities.ui.proveedores
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
+import com.example.freshberries.Configuracion.FreshBerriesBD
 import com.example.freshberries.databinding.FragmentProveedoresBinding
 
 class ProveedoresFragment : Fragment() {
+
+    private lateinit var bd: FreshBerriesBD
 
     private var _binding: FragmentProveedoresBinding? = null
 
@@ -32,6 +38,19 @@ class ProveedoresFragment : Fragment() {
         proveedoresViewModel.text.observe(viewLifecycleOwner) {
 
         }
+
+        val btnRegistrarProveedor : Button = binding.btnRegistrarProveedor
+
+        //Instancia de la Base de datos
+        bd = Room.databaseBuilder(requireContext(), FreshBerriesBD::class.java, FreshBerriesBD.DATABASE_NAME).allowMainThreadQueries().build()
+
+        btnRegistrarProveedor.setOnClickListener {
+            val nextActivity = RegistrarProveedorActivity::class.java
+            val intent = Intent(requireContext(), nextActivity)
+            startActivity(intent)
+        }
+
+
         return root
     }
 
---
+++ app/src/main/java/com/example/freshberries/Activities/ui/proveedores/RegistrarProveedorActivity.kt
@@ -0,0 +1,59 @@
+package com.example.freshberries.Activities.ui.proveedores
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.room.Room
+import com.example.freshberries.Configuracion.FreshBerriesBD
+import com.example.freshberries.Modelo.Proveedor
+import com.example.freshberries.R
+
+class RegistrarProveedorActivity : AppCompatActivity() {
+
+    private lateinit var bd: FreshBerriesBD
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_registrar_proveedor)
+
+        val txtId = findViewById<EditText>(R.id.txtIdProveedor)
+        val txtNombre = findViewById<EditText>(R.id.txtNombreProveedor)
+        val txtTelefono = findViewById<EditText>(R.id.txtTelefonoProveedor)
+        val txtDireccion = findViewById<EditText>(R.id.txtTelefonoProveedor)
+
+        val btnRegistrar = findViewById<Button>(R.id.btnRegistrarProveedor)
+
+        //Instancia de la Base de datos
+        bd = Room.databaseBuilder(application, FreshBerriesBD::class.java, FreshBerriesBD.DATABASE_NAME).allowMainThreadQueries().build()
+
+        btnRegistrar.setOnClickListener {
+            val id = txtId.text.toString().toLongOrNull()
+            val nombre = txtNombre.text.toString()
+            val telefono = txtTelefono.text.toString()
+            val direccion = txtDireccion.text.toString()
+
+            if (id != null && nombre.isNotEmpty() && telefono.isNotEmpty() && direccion.isNotEmpty()){
+                val proveedor = Proveedor(id,nombre,telefono,direccion)
+                try {
+                    bd.proveedorDAO.registrarProveedor(proveedor)
+                }catch (ex : Exception){
+                    Toast.makeText(this, "Ya existe un proveedor con ese ID", Toast.LENGTH_SHORT).show()
+                }
+                Toast.makeText(this, "Se registró el proveedor", Toast.LENGTH_SHORT).show()
+
+                txtId.setText("")
+                txtTelefono.setText("")
+                txtDireccion.setText("")
+                txtNombre.setText("")
+            }else{
+                Toast.makeText(this, "Campos vacíos", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+}
